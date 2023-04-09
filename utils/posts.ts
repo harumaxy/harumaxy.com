c88c7dfa298ab5ad69762b0608be9deaf4093559
harumaxy <harumaxy@gmail.com>
2023-04-10 01:24:29 +0900

    add post util

---
+++ utils/posts.ts
@@ -0,0 +1,40 @@
+import * as front_matter from "$std/encoding/front_matter.ts";
+import * as path from "$std/path/mod.ts";
+import { z } from "https://deno.land/x/zod@v3.21.4/mod.ts";
+
+const __dirname = path.fromFileUrl(import.meta.url);
+const postsDir = path.join(__dirname, "../../posts");
+
+const PostSchema = z.object({
+  slug: z.string(),
+  title: z.string(),
+  published_at: z.string().transform((d) => new Date(d)),
+  snippet: z.string(),
+  content: z.string(),
+});
+
+export type Post = z.infer<typeof PostSchema>;
+
+export async function getPosts(): Promise<Post[]> {
+  const files = Deno.readDir(postsDir);
+  const promises = [];
+  for await (const file of files) {
+    const slug = file.name.replace(".md", "");
+    promises.push(getPost(slug));
+  }
+  const posts = (await Promise.all(promises)) as Post[];
+  const sortedPosts = posts.sort(
+    (a, b) => b.published_at.getTime() - a.published_at.getTime()
+  );
+  return sortedPosts;
+}
+
+export async function getPost(slug: string): Promise<Post | null> {
+  const text = await Deno.readTextFile(path.join(postsDir, `${slug}.md`));
+  const { attrs, body } = front_matter.extract(text);
+  return PostSchema.parse({
+    ...(attrs as Post),
+    slug: slug,
+    content: body,
+  });
+}
