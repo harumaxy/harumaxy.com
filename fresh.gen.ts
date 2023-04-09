dcaae7dd4bc3f1bf0be0f2e89cf3a13ebcfbd469
harumaxy <harumaxy@gmail.com>
2023-04-10 01:24:40 +0900

    generated

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,20 +3,21 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
+import * as $0 from "./routes/[slug].tsx";
+import * as $1 from "./routes/_404.tsx";
+import * as $2 from "./routes/api/joke.ts";
+import * as $3 from "./routes/feeds.ts";
+import * as $4 from "./routes/index.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
+    "./routes/[slug].tsx": $0,
+    "./routes/_404.tsx": $1,
+    "./routes/api/joke.ts": $2,
+    "./routes/feeds.ts": $3,
+    "./routes/index.tsx": $4,
   },
-  islands: {
-    "./islands/Counter.tsx": $$0,
-  },
+  islands: {},
   baseUrl: import.meta.url,
   config,
 };
