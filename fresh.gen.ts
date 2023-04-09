57900937c0b3f88e2ec1f1a71cdb82b6e2ca37c1
harumaxy <harumaxy@gmail.com>
2023-04-10 01:51:10 +0900

    update deps

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,17 +3,17 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/[slug].tsx";
-import * as $1 from "./routes/_404.tsx";
-import * as $2 from "./routes/api/joke.ts";
+import * as $0 from "./routes/_404.tsx";
+import * as $1 from "./routes/api/joke.ts";
+import * as $2 from "./routes/blog/[slug].tsx";
 import * as $3 from "./routes/feeds.ts";
 import * as $4 from "./routes/index.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[slug].tsx": $0,
-    "./routes/_404.tsx": $1,
-    "./routes/api/joke.ts": $2,
+    "./routes/_404.tsx": $0,
+    "./routes/api/joke.ts": $1,
+    "./routes/blog/[slug].tsx": $2,
     "./routes/feeds.ts": $3,
     "./routes/index.tsx": $4,
   },
