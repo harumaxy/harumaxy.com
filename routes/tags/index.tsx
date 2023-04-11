7dc5f8da45188c8060c6197209f7b430b3240287
harumaxy <harumaxy@gmail.com>
2023-04-11 16:48:23 +0900

    fix tags

--- routes/tags/index.tsx
+++ routes/tags/index.tsx
@@ -31,7 +31,7 @@ export default function TagIndex(props: PageProps<TagMap>) {
   return (
     <div class={`${centerContainer} flex flex-col items-center justify-center`}>
       <h1 class="text-5xl font-bold">Tags</h1>
-      <div class="mt-8 flex-wrap">
+      <div class="mt-8 mx-3 flex items-center  flex-wrap">
         {Object.entries(tagMap).map(([tag, count]) => (
           <Tag tag={tag} count={count} key={tag} />
         ))}
