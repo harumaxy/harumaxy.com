66c1e7e5561c98ba9a15648d25f3313578f3102b
harumaxy <harumaxy@gmail.com>
2023-04-13 23:52:51 +0900

    update freshgen

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,12 +7,14 @@ import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/about.tsx";
 import * as $3 from "./routes/api/joke.ts";
-import * as $4 from "./routes/blog/[slug].tsx";
-import * as $5 from "./routes/blog/index.tsx";
-import * as $6 from "./routes/feeds.ts";
-import * as $7 from "./routes/index.tsx";
-import * as $8 from "./routes/tags/[tag].tsx";
-import * as $9 from "./routes/tags/index.tsx";
+import * as $4 from "./routes/api/slides/[slug].ts";
+import * as $5 from "./routes/blog/[slug].tsx";
+import * as $6 from "./routes/blog/index.tsx";
+import * as $7 from "./routes/feeds.ts";
+import * as $8 from "./routes/index.tsx";
+import * as $9 from "./routes/tags/[tag].tsx";
+import * as $10 from "./routes/tags/index.tsx";
+import * as $$0 from "./islands/Slide.tsx";
 
 const manifest = {
   routes: {
@@ -20,14 +22,17 @@ const manifest = {
     "./routes/_app.tsx": $1,
     "./routes/about.tsx": $2,
     "./routes/api/joke.ts": $3,
-    "./routes/blog/[slug].tsx": $4,
-    "./routes/blog/index.tsx": $5,
-    "./routes/feeds.ts": $6,
-    "./routes/index.tsx": $7,
-    "./routes/tags/[tag].tsx": $8,
-    "./routes/tags/index.tsx": $9,
+    "./routes/api/slides/[slug].ts": $4,
+    "./routes/blog/[slug].tsx": $5,
+    "./routes/blog/index.tsx": $6,
+    "./routes/feeds.ts": $7,
+    "./routes/index.tsx": $8,
+    "./routes/tags/[tag].tsx": $9,
+    "./routes/tags/index.tsx": $10,
   },
-  islands: {},
+  islands: {
+    "./islands/Slide.tsx": $$0,
+  },
   baseUrl: import.meta.url,
   config,
 };
