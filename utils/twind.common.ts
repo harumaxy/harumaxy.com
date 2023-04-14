e16901341907d87bf3dcbf76b0a93d69a3bc687c
harumaxy <harumaxy@gmail.com>
2023-04-14 21:24:21 +0900

    fix centor container

--- utils/twind.common.ts
+++ utils/twind.common.ts
@@ -1,1 +1,1 @@
-export const centerContainer = `absolute top-1/2 bottom-1/2 left-0 right-0 m-auto flex items-center justify-center`;
+export const centerContainer = `m-auto flex items-center justify-center`;
