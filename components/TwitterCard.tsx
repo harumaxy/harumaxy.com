9c1579779863d11abc530ea333b5c6213fdff91b
harumaxy <harumaxy@gmail.com>
2023-05-10 14:36:25 +0900

    fix twitter card

--- components/TwitterCard.tsx
+++ components/TwitterCard.tsx
@@ -4,17 +4,12 @@ import { Post } from "../utils/posts.ts";
 export default function TwitterCard({ post }: { post: Post }) {
   return (
     <Head>
-      <meta name="twitter:card" content="summary_card" />
-      <meta name="twitter:site" content="harumaxy.com" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={post.title} />
+      <meta name="twitter:description" content={post.snippet} />
       <meta
-        property="og:url"
-        content={`https://harumaxy.com/blog/${post.slug}`}
-      />
-      <meta property="og:title" content={post.title} />
-      <meta property="og:description" content={post.snippet} />
-      <meta
-        property="og:image"
-        content={`https://avatars.githubusercontent.com/u/15980686?v=4`}
+        property="twitter:image"
+        content="https://avatars.githubusercontent.com/u/15980686?v=4"
       />
     </Head>
   );
