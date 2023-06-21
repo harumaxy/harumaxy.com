4321a81918a2fc13cacaddc3c0826530987493e5
harumaxy <harumaxy@gmail.com>
2023-06-21 19:58:49 +0900

    add marp

--- routes/api/slides/[slug].ts
+++ routes/api/slides/[slug].ts
@@ -1,4 +1,6 @@
 import { HandlerContext } from "$fresh/server.ts";
+import { Marp } from "npm:@marp-team/marp-core";
+import { getPost } from "../../../utils/posts.ts";
 
 const slideRegex = /api\/slides\/(.*)$/g;
 
@@ -6,9 +8,16 @@ export const handler = async (
   req: Request,
   _ctx: HandlerContext
 ): Promise<Response> => {
+  // Convert Markdown slide deck into HTML and CSS
   const slug = Array.from(req.url.matchAll(slideRegex))[0][1];
-  const marpJson = await Deno.readTextFile(`slides/${slug}.json`);
-  return new Response(marpJson, {
+  const post = await getPost(slug);
+  const marp = new Marp();
+
+  const { html: htmls, css } = marp.render(post!.content, {
+    htmlAsArray: true,
+  });
+
+  return new Response(JSON.stringify({ htmls, css }), {
     headers: {
       "content-type": "application/json",
     },
