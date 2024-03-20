ea49774442acadc3c92f3f319a12d7a87abd4b83
harumaxy <harumaxy@gmail.com>
2024-03-20 22:06:43 +0900

    fix routes

--- apps/client/src/index.ts
+++ apps/client/src/index.ts
@@ -4,25 +4,25 @@ import { Home } from "./pages/home";
 // import "@master/normal.css";
 import "@master/css";
 import "./styles/master.css";
+import { Header } from "./components/header";
 
-const { a, div, h1, head, header, html, li, nav, ul } = van.tags;
+const { a, div, h1, head, header, html, li, main, nav, ul } = van.tags;
 
 function App() {
   return div(
-    header(div("Hello, VanJS!"), div("This is a simple VanJS app.")),
-    Router({
-      routes: [
-        { path: "/", component: Home },
-        {
-          path: "/about",
-          component: () => div("About"),
-        },
-        {
-          path: "/contact",
-          component: () => div("Contact"),
-        },
-      ],
-    })
+    Header,
+    main(
+      Router({
+        routes: (
+          [
+            ["/", Home],
+            ["/about", () => div("About")],
+            ["/blog", () => div("Blog")],
+            ["/tags", () => div("Tags")],
+          ] as [string, () => HTMLDivElement][]
+        ).map(([path, component]) => ({ path, component })),
+      })
+    )
   );
 }
 
