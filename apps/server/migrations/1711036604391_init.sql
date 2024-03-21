e248753fffbbc8382b29008a0dbee5196632b1b9
harumaxy <harumaxy@gmail.com>
2024-03-22 01:31:31 +0900

    add init migration

---
+++ apps/server/migrations/1711036604391_init.sql
@@ -0,0 +1,33 @@
+-- Up Migration
+CREATE TABLE
+  users (id SERIAL PRIMARY KEY, username TEXT);
+CREATE TABLE
+  posts (
+    id SERIAL PRIMARY KEY,
+    author_id INT REFERENCES users (id),
+    title TEXT,
+    body TEXT
+  );
+CREATE USER server_app
+WITH
+  LOGIN PASSWORD 'postgres';
+DO $$ BEGIN
+  GRANT SELECT,INSERT,UPDATE,DELETE ON users TO server_app;  -- ALL でもいい
+  GRANT SELECT,INSERT,UPDATE,DELETE ON posts TO server_app;
+  ALTER TABLE users ENABLE ROW LEVEL SECURITY;
+  ALTER TABLE posts ENABLE ROW LEVEL SECURITY;
+  CREATE POLICY only_available_own_user ON users FOR ALL TO server_app USING (
+    id = CURRENT_SETTING('app.uid')::INT OR
+    CURRENT_SETTING('app.role') = 'admin'
+  );
+  CREATE POLICY only_available_own_post ON posts FOR ALL TO server_app USING (
+    author_id = CURRENT_SETTING('app.uid')::INT OR
+    CURRENT_SETTING('app.role') = 'admin'
+  );
+END $$;
+-- Down Migration
+DROP POLICY only_available_own_user ON users;
+DROP POLICY only_available_own_post ON posts;
+DROP TABLE posts;
+DROP TABLE users;
+DROP USER server_app;
