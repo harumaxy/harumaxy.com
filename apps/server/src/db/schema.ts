fbda73e79dbfc08d31d431f046f9377710a88344
harumaxy <harumaxy@gmail.com>
2024-03-21 21:43:09 +0900

    add users

--- apps/server/src/db/schema.ts
+++ apps/server/src/db/schema.ts
@@ -9,6 +9,19 @@ import {
   timestamp,
 } from "drizzle-orm/pg-core";
 
+export const role = pgEnum("role", ["admin", "user"]);
+
+export const users = pgTable(
+  "users",
+  {
+    id: serial("id").primaryKey(),
+    role: role("role").notNull().default("user"),
+  },
+  (users) => {
+    return {};
+  }
+);
+
 export const posts = pgTable(
   "posts",
   {
@@ -18,6 +31,9 @@ export const posts = pgTable(
     content: text("content"),
     published_at: timestamp("published_at", { mode: "date" }).defaultNow(),
     thumbnail: varchar("thumbnail", { length: 256 }),
+    author_id: integer("author_id")
+      .notNull()
+      .references(() => users.id),
   },
   (posts) => {
     return {};
