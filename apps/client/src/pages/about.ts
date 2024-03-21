766238d119348b77bb40c7551c95b99697df6638
harumaxy <harumaxy@gmail.com>
2024-03-21 18:04:22 +0900

    tweak about page

--- apps/client/src/pages/about.ts
+++ apps/client/src/pages/about.ts
@@ -45,12 +45,17 @@ export function About() {
       class:
         "markdown-body p:32 r:10 flex gap:2rem ai:center jc:space-between flex-direction:column flex-direction:row@md",
     },
-    t.img({
-      class: "r:1/2 h:16rem w:16rem",
-      src: "https://avatars.githubusercontent.com/u/15980686?v=4",
-    }),
     t.div(
-      { class: "w:50%" },
+      {
+        class: "flex-grow:1 ai:center jc:center flex",
+      },
+      t.img({
+        class: "r:1/2 h:16rem w:16rem",
+        src: "https://avatars.githubusercontent.com/u/15980686?v=4",
+      })
+    ),
+    t.div(
+      { class: "flex-grow:2 flex-basis:fit-content" },
       Await(
         {
           value: about,
