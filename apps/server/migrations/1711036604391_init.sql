4d4b07e551c056fffc81bfec07c88544616599ee
harumaxy <harumaxy@gmail.com>
2024-03-22 14:05:03 +0900

    fix policy name

--- apps/server/migrations/1711036604391_init.sql
+++ apps/server/migrations/1711036604391_init.sql
@@ -19,15 +19,14 @@ DO $$ BEGIN
   GRANT SELECT,INSERT,UPDATE,DELETE ON posts TO server_app;
   ALTER TABLE users ENABLE ROW LEVEL SECURITY;
   ALTER TABLE posts ENABLE ROW LEVEL SECURITY;
-  CREATE POLICY only_available_own_user ON users FOR ALL TO server_app USING (
+  CREATE POLICY only_available_by_self ON users FOR ALL TO server_app USING (
     CURRENT_SETTING('app.role') = 'admin' OR
     id = CURRENT_SETTING('app.uid')::INT
     
   );
-  CREATE POLICY only_available_own_post ON posts FOR ALL TO server_app USING (
+  CREATE POLICY only_available_by_author ON posts FOR ALL TO server_app USING (
     CURRENT_SETTING('app.role') = 'admin' OR
     author_id = CURRENT_SETTING('app.uid')::INT
-    
   );
 END $$;
 
