e500f69cee7678e982e1e616ac242f6b3594ebac
harumaxy <harumaxy@gmail.com>
2024-03-22 15:38:32 +0900

    fix migration

--- apps/server/migrations/1711072003076_course-and-items.sql
+++ apps/server/migrations/1711072003076_course-and-items.sql
@@ -30,20 +30,33 @@ GRANT ALL ON purchase_of_course_item TO server_app;
 
 ALTER TABLE course_items ENABLE ROW LEVEL SECURITY;
 
-CREATE POLICY only_available_purchased_items ON course_items FOR ALL USING (
-  CURRENT_SETTING('app.role') = 'admin' OR
-  id IN (
-    SELECT
-      item_id
-    FROM
-      purchase_of_course_item
-    WHERE
-      uid = CURRENT_SETTING('app.uid')::INT
-  )
-);
+CREATE POLICY only_available_purchased_items ON course_items FOR
+SELECT
+  USING (
+    CURRENT_SETTING('app.role') = 'admin' OR
+    id IN (
+      SELECT
+        item_id
+      FROM
+        purchase_of_course_item
+      WHERE
+        uid = CURRENT_SETTING('app.uid')::INT
+    )
+  );
+
+DO $$ BEGIN
+  CREATE POLICY admin_can_create_course_items ON course_items FOR INSERT, UPDATE, DELETE
+    USING (CURRENT_SETTING('app.role') = 'admin');
+  CREATE POLICY admin_can_create_courses ON courses FOR INSERT, UPDATE, DELETE
+    USING (CURRENT_SETTING('app.role') = 'admin');
+END $$;
 
 -- Down Migration
 DROP POLICY only_available_purchased_items ON course_items;
+
+DROP POLICY admin_can_create_course_items ON course_items;
+
+DROP POLICY admin_can_create_courses ON courses;
 
 DROP TABLE purchase_of_course_item;
 
