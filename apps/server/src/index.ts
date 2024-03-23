552e1d7c9d93a4de2ca8735e2abd0f894a399cee
harumaxy <harumaxy@gmail.com>
2024-03-23 18:19:03 +0900

    fix server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,13 +1,16 @@
 import { Elysia } from "elysia";
+import { EventContext } from "@cloudflare/workers-types";
 
 const DEFAULT_PORT = 3000;
 
 const app = new Elysia({ aot: false }).get("/", () => "Hello Elysia");
 
-export default {
-  async fetch(req: Request) {
-    return await app.handle(req);
-  },
+export const handle = (app: Elysia) => {
+  return (eventContext: EventContext<{}, string, {}>) => {
+    const { request, env, waitUntil, passThroughOnException } = eventContext;
+    return app.fetch(request as unknown as Request);
+  };
 };
+export const onRequest = handle(app);
 
 export type ServerApp = typeof app;
