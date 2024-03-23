334824d5392702c3c4798a76153fe836344a23f1
harumaxy <harumaxy@gmail.com>
2024-03-23 22:24:37 +0900

    remove old codes

--- apps/server/src/db/client.ts
+++ apps/server/src/db/client.ts
@@ -1,7 +1,5 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/d1";
 import * as schema from "./schema";
-import * as relations from "./relations";
 
-const client = postgres(process.env.DATABASE_URL_FOR_APP!);
-export const db = drizzle(client, { schema: { ...schema, ...relations } });
+export const makeDB = (client: D1Database) => drizzle(client, { schema });
+export type DB = ReturnType<typeof makeDB>;
