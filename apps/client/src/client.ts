3f05e04f20ab9a8ea0db2548c17d6087e42b9dcd
harumaxy <harumaxy@gmail.com>
2024-04-03 19:27:07 +0900

    add cookie option

--- apps/client/src/client.ts
+++ apps/client/src/client.ts
@@ -1,4 +1,8 @@
 import { treaty } from "@elysiajs/eden";
 import type { ServerApp } from "@harumaxy-com/server/src";
 
-export const app = treaty<ServerApp>(window.location.origin);
+export const app = treaty<ServerApp>(window.location.origin, {
+  fetch: {
+    credentials: "same-origin",
+  },
+});
