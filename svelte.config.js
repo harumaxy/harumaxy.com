8ea09a3acdfcf59e96c43a867a7daa110ef22cfb
harumaxy <harumaxy@gmail.com>
2024-07-20 16:09:52 +0900

    ignore cheking origin

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
+		csrf: {
+			checkOrigin: false
+		},
 		adapter: adapter({
 			// See below for an explanation of these options
 			routes: {
