8d4933d005c1cda1a591c9939900aa6fe9f348ab
harumaxy <harumaxy@gmail.com>
2024-07-20 17:43:33 +0900

    fix

--- src/routes/posts/+server.ts
+++ src/routes/posts/+server.ts
@@ -1,78 +1,11 @@
 import { makeDB } from '@/db/client.js';
 import * as schemas from '@/db/schema';
 import { z } from 'zod';
-import matter from 'gray-matter';
+import * as YAML from 'yaml';
 import { eq } from 'drizzle-orm';
 
 export function GET() {
 	return new Response('Method not allowed', { status: 405 });
-}
-
-export async function POST({ platform, request }) {
-	const bearerToken = request.headers.get('Authorization')?.split(' ')[1];
-	if (!bearerToken || bearerToken !== process.env?.['API_KEY']) {
-		return new Response('Unauthorized', { status: 401 });
-	}
-
-	const reqBody = makePostSchema.parse(await request.text());
-	const parsed = matter(reqBody);
-	const content_ = parsed.content.replace(/^\n+/, '');
-	const { slug, title, thumbnail, draft, published_at, description, content, tags } =
-		makePostSchema.parse({
-			...parsed.data,
-			content: content_
-		});
-
-	const d1 = platform?.env.DB;
-	if (!d1) return new Response('No DB', { status: 500 });
-	const db = makeDB(d1);
-
-	await db.transaction(async (db) => {
-		const newTagIds = await db
-			.insert(schemas.tags)
-			.values(tags.map((name) => ({ name })))
-			.returning({
-				id: schemas.tags.id
-			})
-			.onConflictDoNothing({ target: schemas.tags.name });
-
-		const [postId] = await db
-			.insert(schemas.posts)
-			.values([
-				{
-					slug,
-					title,
-					thumbnail,
-					draft,
-					published_at: draft ? published_at : new Date(),
-					description,
-					content
-				}
-			])
-			.returning({
-				id: schemas.posts.id
-			})
-			.onConflictDoUpdate({
-				target: schemas.posts.slug,
-				set: {
-					title,
-					thumbnail,
-					draft,
-					description,
-					content
-				}
-			});
-
-		await db.delete(schemas.postTag).where(eq(schemas.postTag.post_id, postId.id));
-		await db.insert(schemas.postTag).values(
-			newTagIds.map(({ id }) => ({
-				post_id: postId.id,
-				tag_id: id
-			}))
-		);
-	});
-
-	return new Response('Post created', { status: 200 });
 }
 
 const makePostSchema = z.object({
@@ -81,7 +14,81 @@ const makePostSchema = z.object({
 	content: z.string(),
 	thumbnail: z.string().nullable(),
 	draft: z.boolean(),
-	published_at: z.date(),
-	description: z.string(),
+	published_at: z.string().transform((v) => new Date(v)),
+	description: z.string().nullish(),
 	tags: z.array(z.string())
 });
+
+export async function POST({ platform, request }) {
+	const bearerToken = request.headers.get('Authorization')?.split(' ')[1];
+	if (!bearerToken || bearerToken !== platform?.env['API_KEY']) {
+		return new Response('Unauthorized', { status: 401 });
+	}
+
+	const reqBody = await request.text();
+	const [, frontmatter, content_] = reqBody.split('---\n');
+	const parsed = YAML.parse(frontmatter);
+	const zodResult = makePostSchema.safeParse({
+		...parsed,
+		content: content_.replace(/^\n+/, '')
+	});
+	if (!zodResult.success) {
+		return new Response('Invalid post data' + JSON.stringify(zodResult.error), { status: 400 });
+	}
+
+	const { slug, title, thumbnail, draft, published_at, description, content, tags } =
+		zodResult.data;
+
+	const d1 = platform?.env.DB;
+	if (!d1) return new Response('No DB', { status: 500 });
+	const db = makeDB(d1);
+
+	let newTagIds: { id: number }[] = [];
+	if (tags.length > 0) {
+		newTagIds = await db
+			.insert(schemas.tags)
+			.values(tags.map((name) => ({ name })))
+			.returning({
+				id: schemas.tags.id
+			})
+			.onConflictDoNothing({ target: schemas.tags.name });
+	}
+	const [postId] = await db
+		.insert(schemas.posts)
+		.values([
+			{
+				slug,
+				title,
+				thumbnail,
+				draft,
+				published_at: draft ? published_at : new Date(),
+				description,
+				content
+			}
+		])
+		.returning({
+			id: schemas.posts.id
+		})
+		.onConflictDoUpdate({
+			target: schemas.posts.slug,
+			set: {
+				title,
+				thumbnail,
+				draft,
+				description,
+				content
+			}
+		});
+
+	if (newTagIds.length > 0) {
+		await db.delete(schemas.postTag).where(eq(schemas.postTag.post_id, postId.id));
+		await db.insert(schemas.postTag).values(
+			newTagIds.map(({ id }) => ({
+				post_id: postId.id,
+				tag_id: id
+			}))
+		);
+	}
+
+	return new Response('Post created', { status: 200 });
+}
